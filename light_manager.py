e6ffdf57975f0377231afe379d7c94b2bc62149e
Tzion <tzion12345@gmail.com>
2022-12-25 00:58:22 +0200

    create managers and main flow structure

--- light_manager.py
+++ light_manager.py
@@ -1,22 +1,31 @@
-import asyncio
-import enum
+from media_manager import MediaManager
+from traffic_light import Color, TrafficLight
+import random
+import time
 
 
-def start_traffic_light():
-    set_color_
+def start_flow():
+    while True:
+        play_red()
+        play_green()
+        halt()
+
+def play_red():
+    traffic_light.set_color(Color.RED)
+    media_manager.play_red()
+
+def play_green():
+    traffic_light.set_color(Color.GREEN)
+    media_manager.play_green()
+
+
+def halt():
+    one_to_six_minutes_delay = 60.0 + random.random() * 5 * 60
+    delay = one_to_six_minutes_delay
+    time.sleep(delay)
+    
 
 if __name__ == "__main__":
-    asyncio.run(start_traffic_light())
-
-    
-class Color(enum.Enum):
-    RED = "red"
-    GREEN = "green"
-    
-
-class TrafficLight:
-    
-    def __init__(self, initial_color=Color.RED):
-        self.color = initial_color
-
-        
+    traffic_light = TrafficLight()
+    media_manager = MediaManager()
+    start_flow()

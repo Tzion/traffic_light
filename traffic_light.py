81fcc4dfb7fadaaaf0e3d1259fbb278f47aad070
Tzion <zion@demostack.com>
2022-12-27 01:41:55 +0200

    add gpios output

--- traffic_light.py
+++ traffic_light.py
@@ -1,5 +1,8 @@
 from enum import Enum
-# import RPi.GPIO as GPIO
+import RPi.GPIO as GPIO
+
+RED_PIN = 3
+GREEN_PIN = 5
 
 class Color(Enum):
     RED = 0
@@ -11,8 +14,16 @@ class TrafficLight:
         self.color = initialColor
     
     
-    def set_color(self, color):
+    def set_color(self, color: Color):
         print(f'setting color to {color})')
         self.color = color
-        # GPIO.
+        set_gpios(color)
 
+def set_gpios(color_on):
+    if color_on == Color.RED:
+        GPIO.output(RED_PIN, GPIO.HIGH)
+        GPIO.output(GREEN_PIN, GPIO.LOW)
+    if color_on == Color.GREEN:
+        GPIO.output(GREEN_PIN, GPIO.HIGH)
+        GPIO.output(RED_PIN, GPIO.LOW)
+        

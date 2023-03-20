5f03c0405aeda3794826c74f32a0eb7c0ade6c72
Tzion <zion@demostack.com>
2023-03-20 22:07:41 +0200

    handle stuck echo or trigger signal

--- distance.py
+++ distance.py
@@ -14,13 +14,13 @@ GPIO.setup(GPIO_TRIGGER, GPIO.OUT)
 GPIO.setup(GPIO_ECHO, GPIO.IN)
  
 def distance():
+    FunctionStartTime = time.time()
     # set Trigger to HIGH
     GPIO.output(GPIO_TRIGGER, True)
  
     # set Trigger after 0.01ms to LOW
     time.sleep(0.00001)
     GPIO.output(GPIO_TRIGGER, False)
-    FunctionStartTime = time.time()
     StartTime = time.time()
     StopTime = time.time()
  
